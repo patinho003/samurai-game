bf7188d74b58ded7eb0df30fc2b349a8a9a23d80
alemaobonzinh0 <gabrielkp041007@gmail.com>
2024-12-03 16:30:39 -0300

    Testando detecção inimigo/layer

--- Assets/scripts_inimigo/Inimigo.cs
+++ Assets/scripts_inimigo/Inimigo.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Inimigo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject leftPositionObject, rightPositionObject;
+    private float lefPosition, rightPosition;
+    public float speed;
+    
+
+    private  void Start()
     {
-        
+        lefPosition = leftPositionObject.transform.position.x;
+        rightPosition = rightPositionObject.transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        if (transform.position.x < lefPosition)
+        {
+            transform.position = new Vector2 (lefPosition, transform.position.y);
+            speed *= -1;
+        }
+
+        if (transform.position.x > rightPosition)
+        {
+            transform.position = new Vector2 (rightPosition, transform.position.y);
+            speed*= -1;
+        }
     }
+
+
+
+
 }
