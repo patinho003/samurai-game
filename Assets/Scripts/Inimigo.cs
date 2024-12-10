e6cbc225b3a1ccffc209592f28e81b018b4551cd
alemaobonzinh0 <gabrielkp041007@gmail.com>
2024-12-10 12:22:13 -0300

    Movimentação com RigidBody do inimigo

--- Assets/Scripts/Inimigo.cs
+++ Assets/Scripts/Inimigo.cs
@@ -7,30 +7,39 @@ public class Inimigo : MonoBehaviour
     public GameObject leftPositionObject, rightPositionObject;
     private float lefPosition, rightPosition;
     public float speed;
-     private bool isGrounded;
+    private bool isGrounded;
+    private Rigidbody2D rigidBody;
+    private int move;
     
 
     private  void Start()
-    {
+    {   
+        move = 1;
+        rigidBody = GetComponent<Rigidbody2D>();
         lefPosition = leftPositionObject.transform.position.x;
         rightPosition = rightPositionObject.transform.position.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
 
-        if (transform.position.x < lefPosition)
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidBody.velocity = new Vector2(move * speed , rigidBody.velocity.y);
+
+        if (rigidBody.position.x < lefPosition)
         {
-            transform.position = new Vector2 (lefPosition, transform.position.y);
-            speed *= -1;
+            rigidBody.position = new Vector2 (lefPosition, rigidBody.position.y);
+            move = 1;
         }
 
-        if (transform.position.x > rightPosition)
+        if (rigidBody.position.x > rightPosition)
         {
-            transform.position = new Vector2 (rightPosition, transform.position.y);
-            speed*= -1;
+            rigidBody.position = new Vector2 (rightPosition, rigidBody.position.y);
+            move = -1;
         }
     }
 
